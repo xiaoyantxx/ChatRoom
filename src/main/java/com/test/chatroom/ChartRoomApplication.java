9c0357f8a4d3d4deb32086b3e55ad804e05053dd
xiaoyantxx <154036426+xiaoyantxx@users.noreply.github.com>
2024-11-05 16:33:20 +0800

    Add files via upload

---
+++ src/main/java/com/test/chatroom/ChartRoomApplication.java
@@ -0,0 +1,11 @@
+package com.test.chatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartRoomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChartRoomApplication.class,args);
+    }
+}
